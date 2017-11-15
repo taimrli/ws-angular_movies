20e3cb1fb1e901d3b4f523b6ccd5bdbe711b1129
Nicolas Oeschger <nicolas.oeschger@zuehlke.com>
2017-11-15 11:05:58 +0100

    Aufgabe 10.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,21 @@ import {MoviesComponent} from "./cinama/movies/movies.component";
 import {MovieComponent} from "./cinama/movies/movie/movie.component";
 import {MovieService} from "./cinama/movies/movie.service";
 import {HttpClientModule} from "@angular/common/http";
+import {HomeComponent} from "./cinama/home/home.component";
+import {RouterModule, Routes} from "@angular/router";
 
+const routeConfig : Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent, CinemaComponent, MoviesComponent, MovieComponent
+    AppComponent, CinemaComponent, MoviesComponent, MovieComponent, HomeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routeConfig)
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
