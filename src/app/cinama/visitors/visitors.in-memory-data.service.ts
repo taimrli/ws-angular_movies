0a5e6c1d05b7d58187b1f778f4a2adc7ebde1d53
Nicolas Oeschger <nicolas.oeschger@zuehlke.com>
2017-11-17 11:11:10 +0100

    Testing services with HTTP calls

---
+++ src/app/cinama/visitors/visitors.in-memory-data.service.ts
@@ -0,0 +1,13 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Visitor} from "./visitor.model";
+
+export class VisitorsInMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const visitors: Visitor[] = [
+      {id: 1, firstName: 'Peter', lastName: 'Muster'},
+      {id: 2, firstName: 'Petra', lastName: 'Petersen'}
+    ];
+
+    return {visitors}
+  }
+}
